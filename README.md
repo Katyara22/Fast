b92f0c746b762738a58da546966e2dcd84913c43
Katyara22 <irina.pryduvys@gmail.com>
2022-06-11 21:49:41 +0300

    123

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # FastMechanism
+
+npm i -g
+npm i gulp -g
+npm i browser-sync -g
+npm i gulp-sass -g
+npm i gulp-rename -g
+npm i gulp-autoprefixer -g
+npm i gulp-clean-css -g
--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ gulp.task('server', function () {
     });
 });
 
+// completed sass file
 gulp.task('styles', function () {
     return gulp.src("src/assets/sass/style/*.+(scss|sass)")
         .pipe(sass({ autputStyle: 'compressed' }).on('error', sass.logError))
